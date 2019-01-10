3a8f92a0d04eead63a3466bf192f5c989260434e
DeliaOros <delia_iletchi@yahoo.com>
2019-01-10 15:49:54 +0200

    Initial commit

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount regularAccount = new BankAccount("CompanyX", Currency.RON);
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine(regularAccount.ToString());
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine(regularAccount.BalanceAccount);
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Close();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Open();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Deposit();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Withdraw();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Close();
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine(regularAccount.IsClosed);
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Deposit();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Withdraw();
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine(regularAccount.IsActive);
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Open();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Deposit();
+            Console.WriteLine("----------------------------------------------------");
+            regularAccount.Deposit();
+
+
+
+
+
+        }
+    }
+}
